d653c40ed49dcf3667f0d25897517539ab125d08
Daksh Sagar <daksh.sagar@yahoo.com>
2020-09-17 11:48:07 +0530

    Startup

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	h := http.HandlerFunc(Echo)
+
+	log.Println("listening on localhost:8000")
+	if err := http.ListenAndServe("localhost:8000", h); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Echo tells you about the request you made
+func Echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "You asked to", r.Method, r.URL.Path)
+}
